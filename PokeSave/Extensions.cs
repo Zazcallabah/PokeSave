1c9f5819b6c75c3fe0c2f872b44ba342bf2f3dee
Peter Hamberg <zazcallabah@gmail.com>
2013-11-25 00:44:15 +0100

    add mask extension

--- PokeSave/Extensions.cs
+++ PokeSave/Extensions.cs
@@ -4,11 +4,22 @@ namespace PokeSave
 {
 	public static class Extensions
 	{
+		/// <summary>
+		/// origin = 11111111
+		/// mask   = 00001110
+		/// value  = 01010101
+		/// 
+		/// result = 11110101
+		/// </summary>
+		public static uint Mask( this uint origin, uint mask, uint value )
+		{
+			return ( value & mask ) | ( origin & ~mask );
+		}
+
 		public static bool IsSet( this uint val, int pos )
 		{
 			return ( val & ( 1U << pos ) ) != 0;
 		}
-
 
 		public static uint SetBit( this uint val, int pos )
 		{
