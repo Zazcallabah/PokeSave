0e29da39a9043380889c285778bb647b43d973e8
Peter Hamberg <zazcallabah@gmail.com>
2013-12-04 21:58:26 +0100

    chain properties through item list

--- PokeSave/ItemList.cs
+++ PokeSave/ItemList.cs
@@ -2,14 +2,28 @@
 {
 	public static class ItemList
 	{
-		static StringList _items;
+		static readonly StringList Items;
+
+		static ItemList()
+		{
+			if( Items == null )
+				Items = new StringList( "items.bin" );
+		}
 
 		public static string Get( uint index )
 		{
-			if( _items == null )
-				_items = new StringList( "items.bin" );
 
-			return _items.Get( index );
+			return Items.Get( index );
+		}
+
+		public static uint Set( string value )
+		{
+			return Items.First( value );
+		}
+
+		public static string[] All()
+		{
+			return Items.All();
 		}
 	}
 }
