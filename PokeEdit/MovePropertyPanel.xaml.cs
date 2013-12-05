b61717e1e3ff0c3cefb01b1a9d69b0a362517778
Peter Hamberg <zazcallabah@gmail.com>
2013-12-05 12:53:47 +0100

    add fields for team entries

---
+++ PokeEdit/MovePropertyPanel.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace PokeEdit
+{
+	public partial class MovePropertyPanel : UserControl
+	{
+		int _index;
+
+		public MovePropertyPanel()
+		{
+			InitializeComponent();
+		}
+
+		public int Index
+		{
+			get
+			{
+				return _index;
+			}
+			set
+			{
+				if( _index != value )
+				{
+					_index = value;
+					Header.SetBinding( Expander.HeaderProperty, new Binding( "Move" + value + "Name" ) { Mode = BindingMode.OneWay } );
+					Move.PropertyName = "Move" + value;
+					PP.PropertyName = "PP" + value;
+					PPBonus.PropertyName = "PPBonus" + value;
+				}
+			}
+		}
+	}
+}
