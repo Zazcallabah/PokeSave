8525cecc6ccbdf006fa8a65bb75ef590cfdff6bd
Peter Hamberg <zazcallabah@gmail.com>
2013-12-11 09:14:16 +0100

    add hex editor control

---
+++ PokeEdit/Editor.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using PokeSave;
+
+namespace PokeEdit
+{
+	/// <summary>
+	/// Interaction logic for Editor.xaml
+	/// </summary>
+	public partial class Editor : Window
+	{
+		public Editor()
+		{
+			InitializeComponent();
+		}
+
+		void Load( object sender, RoutedEventArgs e )
+		{
+			Text.Text = ( (SaveFile) DataContext ).Latest.Section( Selection.SelectedIndex ).TextRepresentation;
+		}
+		void Save( object sender, RoutedEventArgs e )
+		{
+			( (SaveFile) DataContext ).Latest.Section( Selection.SelectedIndex ).TextRepresentation = Text.Text;
+		}
+	}
+}
