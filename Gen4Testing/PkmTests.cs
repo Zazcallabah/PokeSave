97d6b221584ec37889964112123ce546fc601ac0
Peter Hamberg <zazcallabah@gmail.com>
2014-03-02 22:52:37 +0100

    Start testing gen 4 structures

---
+++ Gen4Testing/PkmTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using PokeSave.Fourth;
+
+namespace Gen4Testing
+{
+	[TestFixture]
+	public class PkmTests
+	{
+		[Test]
+		public void lastpart()
+		{
+			var data = File.ReadAllBytes( "pearlsav.bin" );
+			var pkm = new PokeSave.Fourth.NdsPkm( data, 0x98, false, false );
+			var a =1317105556;
+			Assert.AreEqual( a, pkm.Personality );
+			Assert.AreEqual( 102, pkm.AttackEV );
+		}
+		[Test]
+		public void CanCalculateValidChecksum()
+		{
+			var data = File.ReadAllBytes( "399Bidoof.pkm" );
+			var pkm = new PokeSave.Fourth.NdsPkm( data, 0, true, true );
+
+			Assert.AreEqual( pkm.Checksum, pkm.GenerateChecksum().Value );
+
+		}
+	}
+}
