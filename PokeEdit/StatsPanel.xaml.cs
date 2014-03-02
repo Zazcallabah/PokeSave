f34ab3c68beafb5da4542a739c7a71aebd0b3d64
Peter Hamberg <zazcallabah@gmail.com>
2014-03-02 22:54:03 +0100

    display stats calculation

---
+++ PokeEdit/StatsPanel.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace PokeEdit
+{
+	/// <summary>
+	/// Interaction logic for StatsPanel.xaml
+	/// </summary>
+	public partial class StatsPanel : UserControl
+	{
+		string _propertyName;
+
+		public StatsPanel()
+		{
+			InitializeComponent();
+		}
+
+		public string Stat
+		{
+			get { return _propertyName; }
+			set
+			{
+				if( value != _propertyName )
+				{
+					_propertyName = value;
+					Text.Text = value;
+					IV.SetBinding( TextBox.TextProperty, new Binding( _propertyName + "IV" ) );
+					EV.SetBinding( TextBox.TextProperty, new Binding( _propertyName + "EV" ) );
+					Calc.SetBinding( TextBlock.TextProperty, new Binding( "Calculated" + _propertyName ) );
+					Actual.SetBinding( TextBlock.TextProperty, new Binding( "Current" + _propertyName ) );
+				}
+			}
+		}
+	}
+}
