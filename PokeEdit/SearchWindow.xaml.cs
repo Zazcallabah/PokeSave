725500ab27004a19f2e9904bb5baa0dee21ed98e
Peter Hamberg <zazcallabah@gmail.com>
2014-03-06 15:33:20 +0100

    update searchwindow to use new type information

--- PokeEdit/SearchWindow.xaml.cs
+++ PokeEdit/SearchWindow.xaml.cs
@@ -39,7 +39,8 @@ namespace PokeEdit
 		{
 			_selectedController = "Gen VI";
 			_controllers.Add( "Gen VI", new Gen6SearchController() );
-			_controllers.Add( "Gen III", new Gen6SearchController() );
+			_controllers.Add( "Gen III", new Gen3SearchController() );
+			_controllers.Add( "Gen I", new Gen1SearchController() );
 		}
 
 		public string[] Controllers { get { return _controllers.Keys.ToArray(); } }
@@ -74,152 +75,18 @@ namespace PokeEdit
 
 		ISearchController Controller { get { return _controllers[_selectedController]; } }
 
-		public string Type1
-		{
-			get
-			{
-				return Controller.Types( SelectedName ).First().ToString();
-			}
-		}
-
-		public string Type2
+		public string Types
 		{
 			get
 			{
 				var t = Controller.Types( SelectedName );
-				if( t.Length == 1 || t[0] == t[1] )
-					return string.Empty;
-				return t[1].ToString();
+				if( t.Length == 2 )
+					return t[0] + "\r\n" + t[1];
+				return t[0].ToString();
 			}
 		}
 
-		public string DefStrong1
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName ).First();
-				return Concat( Controller.DefendFilter( t, "½" ) );
-			}
-		}
-
-		public string DefStrong2
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName );
-				if( t.Length == 1 || t[0] == t[1] )
-					return string.Empty;
-				return Concat( Controller.DefendFilter( t[1], "½" ) );
-			}
-		}
-
-		public string AttStrong1
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName ).First();
-				return Concat( Controller.AttackFilter( t, "2" ) );
-			}
-		}
-
-		public string AttStrong2
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName );
-				if( t.Length == 1 || t[0] == t[1] )
-					return string.Empty;
-				return Concat( Controller.AttackFilter( t[1], "2" ) );
-			}
-		}
-		public string DefWeak1
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName ).First();
-				return Concat( Controller.DefendFilter( t, "2" ) );
-			}
-		}
-
-		public string DefWeak2
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName );
-				if( t.Length == 1 || t[0] == t[1] )
-					return string.Empty;
-				return Concat( Controller.DefendFilter( t[1], "2" ) );
-			}
-		}
-
-		public string DefImmune1
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName ).First();
-				return Concat( Controller.DefendFilter( t, "0" ) );
-			}
-		}
-
-		public string DefImmune2
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName );
-				if( t.Length == 1 || t[0] == t[1] )
-					return string.Empty;
-				return Concat( Controller.DefendFilter( t[1], "0" ) );
-			}
-		}
-
-		public string AttWeak1
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName ).First();
-				return Concat( Controller.AttackFilter( t, "½" ) );
-			}
-		}
-
-		public string AttWeak2
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName );
-				if( t.Length == 1 || t[0] == t[1] )
-					return string.Empty;
-				return Concat( Controller.AttackFilter( t[1], "½" ) );
-			}
-		}
-
-		public string AttImmune1
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName ).First();
-				return Concat( Controller.AttackFilter( t, "0" ) );
-			}
-		}
-
-		public string AttImmune2
-		{
-			get
-			{
-				var t = Controller.Types( SelectedName );
-				if( t.Length == 1 || t[0] == t[1] )
-					return string.Empty;
-				return Concat( Controller.AttackFilter( t[1], "0" ) );
-			}
-		}
-
-		string Concat( MonsterType[] types )
-		{
-			var sb = new StringBuilder();
-			foreach( var type in types )
-				sb.AppendLine( type.ToString() );
-			return sb.ToString();
-		}
-
+		public TypeEntry[] Notes { get { return Controller.DefendFilter( Controller.Types( SelectedName ) ); } }
 
 		public event PropertyChangedEventHandler PropertyChanged;
 
@@ -235,61 +102,86 @@ namespace PokeEdit
 	{
 		string[] Names { get; }
 		MonsterType[] Types( string selected );
-		MonsterType[] DefendFilter( MonsterType monsterType, string lookup );
-		MonsterType[] AttackFilter( MonsterType monsterType, string lookup );
+		TypeEntry[] DefendFilter( MonsterType[] monsterType );
 	}
 
-
-	public class Gen6SearchController : ISearchController
+	public class TypeEntry
 	{
-		public Gen6SearchController()
+		static readonly IDictionary<decimal, string> Tagmap = new Dictionary<decimal, string>
 		{
-			Names = Gen6TypeInformation.Names;
+			{0m,"0x"},
+			{0.25m,"¼x"},
+			{.5m,"½x"},
+			{1m,""},
+			{2m,"2x"},
+			{4m,"4x"}
+		};
+		public decimal Weight { get; set; }
+		public string Tag
+		{
+			get
+			{
+				if( Tagmap.ContainsKey( Weight ) )
+					return Tagmap[Weight];
+				return string.Empty;
+			}
+		}
+		public MonsterType Type { get; set; }
+	}
+
+	public abstract class SearchController : ISearchController
+	{
+		readonly TypeChart _chart;
+		readonly ITypeInformation _info;
+		protected SearchController( TypeChart chart, ITypeInformation info )
+		{
+			_chart = chart;
+			_info = info;
 		}
 
-		public string[] Names { get; private set; }
+		public string[] Names { get { return _info.Names; } }
 
 		public MonsterType[] Types( string selected )
 		{
-			return Gen6TypeInformation.GetTypeByName( selected );
+			return _info.GetTypeByName( selected );
 		}
 
-		public MonsterType[] DefendFilter( MonsterType monsterType, string lookup )
+		public TypeEntry[] DefendFilter( MonsterType[] defendingType )
 		{
-			return TypeChart.DefendFilter( monsterType, lookup );
-		}
-
-		public MonsterType[] AttackFilter( MonsterType monsterType, string lookup )
-		{
-			return TypeChart.AttackFilter( monsterType, lookup );
+			var line = _chart.For( defendingType );
+			return _chart.Types.Select( t => new TypeEntry { Type = t, Weight = line.AttackedBy( t ) } ).Where( t => t.Weight != 1 ).ToArray();
 		}
 	}
 
-	public class Gen3SearchController : ISearchController
+	public class Gen1SearchController : SearchController
+	{
+		public Gen1SearchController()
+			: base( new Gen1TypeChart(), new Gen1TypeInformation() )
+		{
+		}
+	}
+	
+	public class Gen3SearchController : SearchController
 	{
 		public Gen3SearchController()
+			: base( new Gen5TypeChart(), new Gen3TypeInformation() )
 		{
-			Names = NameList.All();
 		}
+	}
 
-		public string[] Names { get; private set; }
-
-		public MonsterType[] Types( string selected )
+	public class Gen5SearchController : SearchController
+	{
+		public Gen5SearchController()
+			: base( new Gen5TypeChart(), new Gen5TypeInformation() )
 		{
-			var info = MonsterList.Get( selected );
-			if( info.Type1 == info.Type2 )
-				return new[] { info.Type1 };
-			return new[] { info.Type1, info.Type2 };
 		}
+	}
 
-		public MonsterType[] DefendFilter( MonsterType monsterType, string lookup )
+	public class Gen6SearchController : SearchController
+	{
+		public Gen6SearchController()
+			: base( new Gen6TypeChart(), new Gen6TypeInformation() )
 		{
-			throw new NotImplementedException();
-		}
-
-		public MonsterType[] AttackFilter( MonsterType monsterType, string lookup )
-		{
-			throw new NotImplementedException();
 		}
 	}
 }
